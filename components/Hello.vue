191a46cedac0e5d80d8f380060be76e19795580b
shiropt <63380875+shiropt@users.noreply.github.com>
2021-08-30 09:54:04 +0900

    サンプルファイルを作成

---
+++ components/Hello.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="title">Hello world</div>
+</template>
+<script>
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+<style scoped>
+.title {
+  color: blue;
+}
+</style>
